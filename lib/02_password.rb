bc9d2491fc8ceb4758d91875abee888ad816a2f0
Greg Rouxel Oldrà <rouxel.g@gmail.com>
2019-01-10 22:40:15 +0100

    la bonne répionse du 02 :D

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,33 +1,33 @@
-def signup
-  puts "Merci de définir votre mot de pass"
-  print"> "
-  password = gets.chomp.to_s
-  return password
-end
+def signup()
+    puts "Please choose your password"
+    print"> "
+    @password = gets.chomp.to_s
+  end
+
+  def login()
+    puts "Confirm your password"
+    print "> "
+    confirm = gets.chomp.to_s
+      if confirm == @password
+        welcome_screen()
+      else
+        login()
+      end
+  end
+
+  def welcome_screen()
+    puts "Bienvenue à toi jeune prodige du code"
+    puts "J'ai accès aux Whatsapp de tous les prodiges de ton groupe'"
+    puts "Mais surtout n'en parle à personne"
+    puts "Felix et Charles fleurtent avec Amadou"
+    puts "Mais Amadou est amoureux de LebronJames :D"
+  end
 
 
 
-def login
-puts "confirm your pass"
-cofirm = gets.chomp.to_s
-end
-
-
-
-def perform
-  a = signup()
-  b = login()
-    while a != b
-      puts "incorrect password"
-      b = login
-    end
-  welcome_screen
-end
-
-def welcome_screen
-  puts "heyy mon coco bienvenue"
-
-end
-
+  def perform
+      signup()
+      login()
+  end
 
 perform
