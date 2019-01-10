aefcb88a71616dbf9686ada4904b3281a32582dd
Greg Rouxel Oldrà <rouxel.g@gmail.com>
2019-01-10 17:50:35 +0100

    Vacances en Egypte

--- lib/01_pyramid.rb
+++ lib/01_pyramid.rb
@@ -13,7 +13,7 @@ def half_pyramid
 
 
 def full_pyramid
-  puts "Salut, bienvenue dans ma super pyramide ! Choissis un nombre en 1 et 25."
+  puts "Salut, bienvenue dans ma MEGApyramide ! Choissis un nombre en 1 et 25."
 print ">"
 etages = gets.chomp.to_i
 x = " "
@@ -28,11 +28,32 @@ etages.times do |a|
 end
 end
 
+def wtf_pyramid
+  puts "Salut, bienvenue dans ma MEGApyramide ! Choissis un nombre en 1 et 25."
+print ">"
+etages = gets.chomp.to_i
+x = " "
+y = 1
+z = "#"
+
+
+puts "Voici la pyramide :"
+
+etages.times do |a|
+  if a <= etages/2
+    puts x*(etages-a-1)+"#"*a+z*y
+    y +=1
+  else
+    print (' '*a)
+    puts '#'*((etages*2-(a*2))-1)
+
+end
+end
+end
 
 
 def perform
-half_pyramid
-full_pyramid
+wtf_pyramid
 end
 
 perform
