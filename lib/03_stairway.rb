961c90cb8af20dc779ec627aa56ed5d50cac4358
Greg Rouxel Oldrà <rouxel.g@gmail.com>
2019-01-11 01:09:46 +0100

    Merci MAX

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,30 +1,41 @@
-
-def gameon()
-  return rand(1..6)
-  if rand(1..6) > 5
-    x += 1
-    puts "Vous avancez d'une case!"
-    puts "Vous êtes sur la case numéro #{x}"
-  until x == 10
-    puts "You are on the top of the world!"
-    gameon()
+def jeu
+    position = [1]
+    i = 0
+    while i < 9
+        x = rand(1..6)
+        puts "Ton lancé de dé est #{x}"
+        if x >= 5
+            position.push(position.count + 1)
+            puts "..."
+            puts "Tu avances de 1 marche, voici ta position : #{position}"
+            i += 1
+            puts i
+        elsif x >= 2 && x < 5
+            puts "..."
+            puts "Rien ne se passe, voici où tu en es #{position}"
+        else x == 1
+            position.slice!(-1)
+            puts "..."
+            puts "Tu descends de 1 marche, voici ta position : #{position}"
+                if i == 0
+                    puts "Désolé, tu as perdu le jeu..."
+                    break
+                elsif i > 0
+                    i -= 1
+                end
+        end
     end
-  elsif rand(1..6) >= 2 && result <= 4
-    x = x + 0
-    puts "Blanc sur rouge, rien ne bouge!"
-    puts "Vous êtes sur la case numéro #{x}"
-    gameon()
-  else rand(1..6) == 1
-    x -= 1
-    puts "OOh, vous reculez d'une case"
-    puts "Vous êtes sur la case numéro #{x}"
-    gameon()
-  end
+
+    if i == 9
+        puts "..."
+        puts "!!!"
+        puts "Tu as gagné !!!!!"
+    else
+    end
 end
 
-
 def perform
-gameon()
+    jeu
 end
 
 perform
